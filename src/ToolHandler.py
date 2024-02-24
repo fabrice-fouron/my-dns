fdf7c8ed40a5c93e04a1f3b263b6cfdf222da2df
Fabrice Fouron <fouronfp05@gmail.com>
2024-02-24 15:23:07 -0500

    working on the project

---
+++ src/ToolHandler.py
@@ -0,0 +1,2 @@
+class ToolHandler:
+    pass
---
+++ src/User.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, username:str, first_name:str, last_name:str) -> None:
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def login():
+        pass
