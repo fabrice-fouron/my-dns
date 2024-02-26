f0ac097067e1a4247090143e25a03f0575fa404b
Fabrice Fouron <fouronfp05@gmail.com>
2024-02-26 11:28:53 -0500

    some progress made

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my-dns
+
+This project is an attempt to bind 2 great features together : To-Do list and screen time
+
+## Partitions
+There are multiple parts to this project.
+
+First there is the server side. The server handles the database which will take care of storing the information about users, activities and tasks for each user.
+
--- src/User.py
+++ src/User.py
@@ -1,8 +1,12 @@
 class User:
-    def __init__(self, username:str, first_name:str, last_name:str) -> None:
+    def __init__(self, username:str, first_name:str, last_name:str, email:str) -> None:
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
 
-    def login():
-        pass
+    def get_username(self) -> str:
+        return self.username
+    
+    def get_fullname(self) -> str:
+        return self.first_name + " " + self.last_name
